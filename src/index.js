817abd30b2e85fc89720e20f1185b549740c1c2a
stefi23 <stefania.rosca2309@gmail.com>
2020-07-21 09:01:13 +0200

    first part of the tutorial

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ import "./index.css";
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    (this.rows = 30), (this.cols = 50);
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Arrray(this.cols).fill(false)),
     };
   }
 
@@ -16,7 +21,12 @@ class Main extends React.Component {
     return (
       <div>
         <h1>Game of Life</h1>
-        <Grid />
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
@@ -25,7 +35,31 @@ class Main extends React.Component {
 
 class Grid extends React.Component {
   render() {
-    return <div>Grid</div>;
+    const width = this.prop.cols * 14;
+    const rowsArr = [];
+    let boxClass = "";
+
+    for (let i = 0; 0 < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+    return (
+      <div className="grid" style={{ width: width }}>
+        {{ rowsArr }}
+      </div>
+    );
   }
 }
 
